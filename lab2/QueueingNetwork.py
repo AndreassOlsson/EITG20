82085bf0b05cddb5c931bd0cf755fbc49858ed84
Andreas <andreas.o.olsson@outlook.com>
2026-05-20 11:24:09 +0200

    Import source code from nikolai

--- lab2/QueueingNetwork.py
+++ lab2/QueueingNetwork.py
@@ -5,8 +5,10 @@ from queue import Queue
 
 signalList = []
 
+
 def send(signalType, evTime, destination, info):
     heapq.heappush(signalList, (evTime, signalType, destination, info))
+
 
 GENERATE = 1
 ARRIVAL = 2
@@ -14,10 +16,10 @@ MEASUREMENT = 3
 DEPARTURE = 4
 
 simTime = 0.0
-stopTime = 100000.0
-        
+stopTime = 1000.0
 
-class larger():
+
+class larger:
     def __gt__(self, other):
         return False
 
@@ -26,55 +28,81 @@ class generator(larger):
     def __init__(self, lambda1):
         self.lambda1 = lambda1
         send(GENERATE, self.arrivalTime(), self, None)
+
     def arrivalTime(self):
         return simTime + random.expovariate(self.lambda1)
+
     def treatSignal(self, x, info):
         if x == GENERATE:
             send(ARRIVAL, simTime, sendTo(self), simTime)
             send(GENERATE, self.arrivalTime(), self, None)
 
 
-
 class queue(larger):
     def __init__(self, mu):
         self.measuredValues = []
+        self.Nq = []  # ADDED: List for customers strictly in the buffer
+        self.Ts = []  # ADDED: List for service times
         self.buffer = Queue(maxsize=0)
-        self.mu = mu 
+        self.mu = mu
         send(MEASUREMENT, simTime + random.expovariate(1), self, None)
+
     def serviceTime(self):
-        return simTime + random.expovariate(self.mu)
+        alpha = 1.0 / 1.5
+        mu_1 = 2.0 * self.mu
+        mu_2 = self.mu / 2.0
+
+        # Flip a weighted coin to choose the service rate
+        if random.random() < alpha:
+            service_time = random.expovariate(mu_1)
+        else:
+            service_time = random.expovariate(mu_2)
+
+        self.Ts.append(service_time)
+        return simTime + service_time
+
     def treatSignal(self, x, info):
-        if x == ARRIVAL: 
+        if x == ARRIVAL:
             if self.buffer.empty():
-                send(DEPARTURE, self.serviceTime(), self, None) 
+                send(DEPARTURE, self.serviceTime(), self, None)
             self.buffer.put(info)
         elif x == DEPARTURE:
             tid = self.buffer.get()
             send(ARRIVAL, simTime, sendTo(self), tid)
             if not self.buffer.empty():
-                send(DEPARTURE,  self.serviceTime(), self, None)  
+                send(DEPARTURE, self.serviceTime(), self, None)
         elif x == MEASUREMENT:
             self.measuredValues.append(self.buffer.qsize())
+
+            # ADDED: Measure only the buffer
+            if self.buffer.qsize() > 0:
+                self.Nq.append(self.buffer.qsize() - 1)
+            else:
+                self.Nq.append(0)
+
             send(MEASUREMENT, simTime + random.expovariate(1), self, [])
 
-            
+
 class sink(larger):
     def __init__(self):
-        self.T = [] 
+        self.T = []
+
     def treatSignal(self, x, info):
         self.T.append(simTime - info)
 
-# Below the queueing network is set up. 
 
-# A vector where q[i] is node i is created. The first position in the list is not 
-# used so that q[i] correspondst to node number i, for convenience.  
-q = [None, queue(10), queue(14), queue(22), queue(9), queue(11)] 
+# Below the queueing network is set up.
+
+# A vector where q[i] is node i is created. The first position in the list is not
+# used so that q[i] correspondst to node number i, for convenience.
+q = [None, queue(10), queue(14), queue(22), queue(9), queue(11)]
 
 # Sinks and generators are created.
 sink1 = sink()
 sink2 = sink()
 gen1 = generator(7.5)
 gen2 = generator(10)
+
 
 # The function sendTo(source) gives the routing in the queueing network
 def sendTo(source):
@@ -102,13 +130,29 @@ while simTime < stopTime:
     [simTime, signalType, dest, info] = heapq.heappop(signalList)
     dest.treatSignal(signalType, info)
 
-# The mean number of customers in each node i printed:  
-for i in range(1,6):
-    print(i, ': ', np.mean(q[i].measuredValues))
-    
+print(f"E(N_1q): {np.mean(q[1].Nq)}")
+print(f"E(N_3q): {np.mean(q[3].Nq)}")
+print(f"E(T_3s): {np.mean(q[3].Ts)}")
+print(f"E(T_4s): {np.mean(q[4].Ts)}")
 
-    
+total_times = sink1.T + sink2.T
+print("Simulated total T:", np.mean(total_times))
 
+# --- Verifying Little's Theorem ---
+# 1. Calculate total E(N) by summing the mean of measuredValues for all 5 queues
+E_N_total = sum(np.mean(q[i].measuredValues) for i in range(1, 6))
 
+# 2. Grab E(T) which is the mean of total_times
+E_T = np.mean(total_times)
 
+# 3. Define our total arrival rate (Lambda_eff)
+lambda_eff = 7.5 + 10.0
 
+# 4. Print them out to compare!
+print("\n--- Little's Theorem Verification ---")
+print(f"Total E(N): {E_N_total}")
+print(f"Lambda_eff * E(T): {lambda_eff * E_T}")
+
+# The mean number of customers in each node i printed:
+for i in range(1, 6):
+    print(i, ": ", np.mean(q[i].measuredValues))
--- lab2/dispatcher.py
+++ lab2/dispatcher.py
@@ -1,5 +1,6 @@
 import heapq
 import random
+
 # If you are a Mac user, uncomment the two following lines:
 # import matplotlib as mpl
 # mpl.use('tkagg')
@@ -12,9 +13,10 @@ import sys
 
 signalList = []
 
+
 def send(signalType, evTime, destination, info):
     heapq.heappush(signalList, [evTime, signalType, destination, info])
-    
+
 
 GENERATE = 1
 ARRIVAL = 2
@@ -25,18 +27,20 @@ MEASUREMENT = 4
 simTime = 0.0
 stopTime = 100000
 
-    
-class larger():
+
+class larger:
     def __gt__(self, other):
         return False
-        
+
 
 class generator(larger):
     def __init__(self, lamda, sendTo):
         self.sendTo = sendTo
         self.lamda = lamda
+
     def arrivalTime(self):
         return simTime + random.expovariate(self.lamda)
+
     def treatSignal(self, x, info):
         if x == GENERATE:
             send(ARRIVAL, simTime, self.sendTo, [simTime])
@@ -47,12 +51,15 @@ class dispatcher(larger):
     def __init__(self, queues):
         self.queues = queues
         self.rr = 0
+
     def randomDispatch(self):
-        # Here code should be added
-        return 
+        # randomly select one queue from the list of queues
+        return random.choice(self.queues)
+
     def roundRobin(self):
         self.rr = (self.rr + 1) % len(self.queues)
         return self.queues[self.rr]
+
     def fewestCustomers(self):
         smallest = 0
         smallestNumber = sys.maxsize
@@ -61,17 +68,14 @@ class dispatcher(larger):
                 smallest = i
                 smallestNumber = self.queues[i].numberInQueue
         return self.queues[smallest]
-        
+
     def treatSignal(self, x, info):
         if x == ARRIVAL:
-            send(ARRIVAL, simTime, self.roundRobin(), [simTime])
-       
+            send(ARRIVAL, simTime, self.fewestCustomers(), [simTime])
 
-            
-            
-            
+
 class queue(larger):
-    def __init__(self,mu, sendTo):
+    def __init__(self, mu, sendTo):
         self.numberInQueue = 0
         self.sumMeasurements = 0
         self.numberOfMeasurements = 0
@@ -79,9 +83,11 @@ class queue(larger):
         self.buffer = Queue(maxsize=0)
         self.sendTo = sendTo
         self.mu = mu
+
     def serviceTime(self):
-         return simTime + random.expovariate(self.mu)
-        #return 1/self.mu
+        return simTime + random.expovariate(self.mu)
+
+    # return 1/self.mu
     def treatSignal(self, x, info):
         if x == ARRIVAL:
             if self.numberInQueue == 0:
@@ -99,67 +105,80 @@ class queue(larger):
             self.numberOfMeasurements = self.numberOfMeasurements + 1
             send(MEASUREMENT, simTime + 1.0, self, [])
 
-            
+
 class sink(larger):
     def __init__(self):
         self.numberArrived = 0
         self.totalTime = 0
+
     def treatSignal(self, x, info):
         self.numberArrived = self.numberArrived + 1
         self.totalTime = self.totalTime + simTime - info[0]
 
 
-startTime = time.time()           
+startTime = time.time()
 s = sink()
-# Here the queuing systems are created and their service rates are given values
-queues = [queue(MU,s) for MU in [1, 1, 1, 1, 1]]
+# Updated service rates: mu0=1, mu1=0.5, mu2=1.5, mu3=1, mu4=1
+queues = [queue(MU, s) for MU in [1, 0.5, 1.5, 1, 1]]
 disp = dispatcher(queues)
-gen = generator(4,disp)
+gen = generator(4, disp)
 
 send(GENERATE, 0, gen, [])
 for i in queues:
     send(MEASUREMENT, 1, i, [])
 
-    
+
 while simTime < stopTime:
     [simTime, signalType, dest, info] = heapq.heappop(signalList)
     dest.treatSignal(signalType, info)
 
 
-
-
 Lq = len(queues)
 for i in range(Lq):
-    print('In queueing system ',i,': ', queues[i].sumMeasurements/queues[i].numberOfMeasurements)
+    print(
+        "In queueing system ",
+        i,
+        ": ",
+        queues[i].sumMeasurements / queues[i].numberOfMeasurements,
+    )
 
 
-print('Mean time in queue: ', s.totalTime/s.numberArrived)
+print("Mean time in queue: ", s.totalTime / s.numberArrived)
 
 
 totalTid = time.time() - startTime
-print('Elapsed time: ', totalTid)
+print("Elapsed time: ", totalTid)
 
 
 x = 20
-a =  list(range(0, x))
-hist = [np.histogram(queues[i].measuredValues, bins=a,density=True)[0] for i in range(Lq)]
+a = list(range(0, x))
+hist = [
+    np.histogram(queues[i].measuredValues, bins=a, density=True)[0] for i in range(Lq)
+]
 
 
-
-
-
-X = np.arange(x-1)
+X = np.arange(x - 1)
 fig = plt.figure()
 a = 0.00
 w = 0.15
 
-for i in range(len(queues)): 
-    plt.bar(X + a, hist[i], width = w)
+for i in range(len(queues)):
+    plt.bar(X + a, hist[i], width=w)
     a += w
 
+# --- Calculate Total Metrics ---
 
+# Sum the average number of customers from every queue in the dispatcher
+total_E_N = sum(q.sumMeasurements / q.numberOfMeasurements for q in queues)
 
+# The sink already has the total time and total count for E(T)
+mean_E_T = s.totalTime / s.numberArrived
 
+print(f"\nTotal mean number of customers E(N): {total_E_N:.4f}")
+print(f"Mean time in system E(T): {mean_E_T:.4f}")
+
+# Optional: Verify with Little's Law (E(N) = lambda * E(T))
+# lambda is 4 in your current setup
+print(f"Verification (Lambda * E(T)): {4 * mean_E_T:.4f}")
 
 plt.show()
-
